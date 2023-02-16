af8dc3a3a8299dc7c50000ccaaae3410f08ca478
ekaterina <whitedevil93@mail.ru>
2023-02-16 22:32:05 +0300

    точка пересечения

--- дз5.1/Program.cs
+++ дз5.1/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
 
-
-
 string X(int k1, int k2, int b1, int b2)
 {
     string result = string.Empty;
