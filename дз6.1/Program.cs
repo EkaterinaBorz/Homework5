61ccabb653aa640a5996a950d401eced04197c27
ekaterina <whitedevil93@mail.ru>
2023-02-16 22:47:38 +0300

    двумерный массив mn

---
+++ дз6.1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.Clear();
+Console.WriteLine();
+void FillArray(double[,] array)                                          
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble();
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 6;
+int n = 3;
+double[,] array = new double [m, n];
+
+FillArray(array);
+PrintArray(array);
