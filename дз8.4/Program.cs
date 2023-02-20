c4b0db3be2d85ab29ea47ba8508d995be7eb3113
ekaterina <whitedevil93@mail.ru>
2023-02-20 18:06:18 +0300

    трехмерный массив

---
+++ дз8.4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+
+int[,,] array = new int[3, 3, 3];
+FillArray(array);
+PrintIndex(array);
+
+
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int m = 0; m < array.GetLength(2); m++)
+            {
+                Console.Write($"{array[i, j, m]}({i},{j},{m}) ");
+            }
+        }
+    }
+}
+
+
+void FillArray(int[,,] arr)
+{
+    int count = 20;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int m = 0; m < arr.GetLength(2); m++)
+            {
+                arr[m, i, j] += count;
+                count += 4;
+            }
+        }
+    }
+}
